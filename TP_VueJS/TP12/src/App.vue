8a42c52bcc663f244aa1eacb6f6e9b80085e94bb
Soaen <sylvain.vansteenkiste59@gmail.com>
2023-07-20 16:08:50 +0200

    Exercice 12

---
+++ TP_VueJS/TP12/src/App.vue
@@ -0,0 +1,56 @@
+<script setup>
+
+import Alert from './components/Alert.vue'
+
+</script>
+
+<template>
+
+  <Alert>
+
+    <template v-slot:alerttitle>
+      
+      Super Alerte ! 
+
+    </template>
+    <template v-slot:alerttext>
+      
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Porro, aliquid. 
+
+    </template>
+
+  </Alert>
+
+
+  <Alert>
+
+    <template v-slot:alerttitle>
+      
+      Méga Alerte ! 
+
+    </template>
+    <template v-slot:alerttext>
+      
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Porro, aliquid. 
+
+    </template>
+
+  </Alert>
+
+
+  <Alert>
+
+    <template v-slot:alerttitle>
+      
+      La classique
+
+    </template>
+    <template v-slot:alerttext>
+      
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Porro, aliquid. 
+
+    </template>
+
+  </Alert>
+
+</template>
---
+++ TP_VueJS/TP12/src/components/Alert.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="alert-box">
+        <h2 class="alert-title">
+            <slot name="alerttitle"></slot>
+
+        </h2>
+        <p>
+            <slot name="alerttext"></slot>
+        </p> 
+    </div>
+</template>
+
+<style>
+
+.alert-box{
+    background-color: red;
+    width: 50%;
+    padding: 25px;
+    color: white;
+    margin: 20px;
+}
+.alert-title{
+    font-weight: bold;
+}
+
+</style>
