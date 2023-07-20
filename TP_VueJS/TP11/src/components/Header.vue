bd756155f86a0017d912ec0aa665afb35b8610cb
Soaen <sylvain.vansteenkiste59@gmail.com>
2023-07-20 11:28:02 +0200

    Exercice 11

---
+++ TP_VueJS/TP11/src/components/Header.vue
@@ -0,0 +1,17 @@
+<script setup>
+
+import Menu from './Menu.vue'
+
+</script>
+
+
+<template>
+
+    <header>
+
+        <img src="logo.jpg" alt="Logo" width="50">
+        <Menu></Menu>
+
+    </header>
+
+</template>
---
+++ TP_VueJS/TP11/src/components/Menu.vue
@@ -0,0 +1,23 @@
+<script setup>
+
+
+</script>
+
+
+<template>
+
+    <nav>
+
+        <ul>
+            
+            <li><a href="#">Accueil</a></li>
+            <li><a href="#">Boutique</a></li>
+            <li><a href="#">Contact</a></li>
+            <li><a href="#">CGV</a></li>
+            
+        </ul>
+
+    </nav>
+
+</template>
+
